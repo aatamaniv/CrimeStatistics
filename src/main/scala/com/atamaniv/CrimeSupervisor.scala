6fdd851cd48b096b4d0dc4ce38c75d5f5c893f9d
Andriy Atamaniv <andriy.atamaniv@gmail.com>
2019-03-29 00:39:10 +0200

    Story-1: refactoring

--- src/main/scala/com/atamaniv/CrimeSupervisor.scala
+++ src/main/scala/com/atamaniv/CrimeSupervisor.scala
@@ -12,10 +12,16 @@ class CrimeSupervisor extends Actor with ActorLogging {
   override def postStop(): Unit = log.info("Crime supervisor stopped")
 
   override def receive: Receive = {
-    case CreateFileReaderActor(path) => sender ! context.actorOf(FileReader.props(), "ActorOfFile_" + path)
-    case CreateFolderReaderActor(path) => sender ! context.actorOf(FolderReader.props(), "ActorOfFolder_" + path)
-    case FolderReaderCreated(ref) => ref ! GetCsvFiles("resources.crimes")
+    case CreateFileReaderActor(path) => sender ! FileReaderCreated(context.actorOf(FileReader.props(), "ActorOfFile_" + path))
+
+    case CreateFolderReaderActor(path) => log.info(s"asked for Folder Reader on path $path")
+      sender ! FolderReaderCreated(context.actorOf(FolderReader.props(), "ActorOfFolder_" + path))
+
+    case FolderReaderCreated(ref) => log.info(s"received folderReader: $ref")
+      ref ! GetCsvFiles("resources.crimes")
+
     case CsvFiles(files) => files.foreach(self ! CreateFileReaderActor(_))
+
     case PrintMessage(message) => println(message)
   }
 }
